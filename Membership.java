7db6e045adab56c43b04261198ca85c4371cbaaa
Hadi Farhat <hadi236a@gmail.com>
2021-01-06 09:17:43 +0000

    Create Membership.java

---
+++ Membership.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Membership {
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+    public static DateTimeFormatter dateFormatter =  DateTimeFormatter.ofPattern("dd/MM/yy");
+    private String type;
+    private int price;
+}
